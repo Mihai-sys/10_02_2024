d14d5f9691a8887c82681bd8ecf35f10e61ae535
Mihai-sys <mihaicostros7@gmail.com>
2024-10-02 20:19:49 +0200

    add variables

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+
+// vzriabila declarata si apoi atribuita valoare
+let userName;
+userName = "Monica";
+console.log(userName);
+userName = "_monica_";
+console.log(userName);
+
+// variabila declarata si initiata cu o valoare intr-o linie
+let email = "user@test.com";
+console.log(email);
+
+
+// constanta declarata cu valoare
+const pi = 3.14;
+// interzisa reatribuirea de valori unei constante
